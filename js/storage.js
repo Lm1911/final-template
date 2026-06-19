4b5a34d30e207224f482f6815567ff1d6caf258c
Lm1911 <l_maghradze@cu.edu.ge>
2026-06-20 01:34:59 +0400

    Update storage.js

---
+++ js/storage.js
@@ -0,0 +1,32 @@
+const STORAGE_KEY = "moodshelf-saved-books";
+
+export function getSavedBooks() {
+  const savedBooks = localStorage.getItem(STORAGE_KEY);
+
+  if (savedBooks === null) {
+    return [];
+  }
+
+  return JSON.parse(savedBooks);
+}
+
+export function saveBooks(books) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(books));
+}
+
+export function addSavedBook(book) {
+  const savedBooks = getSavedBooks();
+
+  const bookAlreadyExists = savedBooks.some(function (savedBook) {
+    return savedBook.key === book.key;
+  });
+
+  if (bookAlreadyExists) {
+    return false;
+  }
+
+  savedBooks.push(book);
+  saveBooks(savedBooks);
+
+  return true;
+}
